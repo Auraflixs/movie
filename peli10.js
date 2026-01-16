27fddd1156f93cb079fec06b81b4de94d6998cc3
yoldkwlws <lestheradonys@gmail.com>
2026-01-16 17:29:30 -0600

    Add files via upload

---
+++ peli10.js
@@ -0,0 +1,13 @@
+// weapons.js - La hora de la desaparición (Weapons)
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p10",
+    title: "La hora de la desaparición",
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/cwjNSLj5FCkwFsTx7g57ybWQDcp.jpg",
+    trailer: "https://www.youtube.com/embed/Xf0mVoCgiko?start=8",
+    video: "https://vimeos.net/embed-fu1czv6eud18.html",
+    year: 2025,
+    info: "Una épica de terror interconectada y de múltiples historias sobre la desaparición de niños en un pequeño pueblo, explorando el miedo y lo sobrenatural.",
+    featured: true
+});
