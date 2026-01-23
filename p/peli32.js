d2890cb2d7db75c4ea758ef730ba236910a12d26
Auraflix <lestheradonys@gmail.com>
2026-01-23 16:07:36 -0600

    Add files via upload

---
+++ p/peli32.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p32",
+    title: "La sustancia",
+    tags: ["The Substance"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/w1PiIqM89r4AM7CiMEP4VLCEFUn.jpg",
+    trailer: "https://www.youtube.com/embed/bb10YAiF8nE",
+    video: "https://vimeos.net/embed-se641y39hdnm.html",
+    year: 2024,
+    genres: ["Terror", "Ciencia ficción", "Suspenso"],
+    info: "Elisabeth Sparkle, una celebridad en decadencia, decide utilizar una droga del mercado negro que replica las células y crea temporalmente una versión más joven y perfecta de sí misma. Sin embargo, la convivencia con su alter ego comienza a generar efectos secundarios aterradores cuando las reglas de equilibrio entre ambos cuerpos se rompen, desatando una espiral de horror corporal y obsesión por la belleza.",
+    featured: true
+});
