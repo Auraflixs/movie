34d3f72b53050a027b5b7931c55c87cb0472d27a
Auraflix <lestheradonys@gmail.com>
2026-01-22 22:25:23 -0600

    Add files via upload

---
+++ p/peli22.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p22",
+    title: "El Botín",
+    tags: ["The Rip"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/55C5loKFkw7Cj0nMLWux4FJCRHI(1).jpg",
+    trailer: "https://www.youtube.com/embed/c7dGOpMFFeA?start=1",
+    video: "https://vimeos.net/embed-9b7uadbng1z9.html",
+    year: 2026,
+    genres: ["Acción", "Suspenso", "Crimen"],
+    info: "Un grupo de expertos ladrones planea el atraco más ambicioso de la década: infiltrarse en una de las bóvedas más seguras del mundo durante un evento de gala. Lo que comienza como un plan perfecto se complica cuando una traición interna y la llegada inesperada de fuerzas de seguridad convierten el robo en una lucha desesperada por escapar con vida y el botín en sus manos.",
+    featured: true
+});
