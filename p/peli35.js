de242da252c925426f66813d72dfd95fdb2edec4
Auraflix <lestheradonys@gmail.com>
2026-01-24 12:29:19 -0600

    Update peli35.js

--- p/peli35.js
+++ p/peli35.js
@@ -6,7 +6,7 @@ window.moviesList.push({
     tags: ["juegos de niños", "Playdate"],
     image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/nWprcqzpw41jslTgGrGcf4QhZ7Z.jpg",
     trailer: "https://www.youtube.com/embed/AfhRi520IJw",
-    video: "https://vimeos.net/embed-erbu1wnxq49z.html",
+    video: "https://s291klg.storage.yandex.net/rdisk/54a774603f0421da7db69f4a46db71e9b336d8787b1ecb328fddc1d2fbaaed2c/69754624/9E8LvVRYrvJ9Ypy7DCbq8tzfhm3fizK1DQ40bQmg0jqKg3OtxzGabgtOMXDllesB79SMDgSJY7NMZZv7yzZw6Q==?uid=0&filename=MLPLMDLA%20S01E02.mp4&disposition=inline&hash=IV9O831ClKchnuQ/wYF5EuWN4fjiv045BbbCn5CchHvYle2anzhRVRp4ijfNBkTTq/J6bpmRyOJonT3VoXnDag%3D%3D&limit=0&content_type=video%2Fmp4&owner_uid=1762222981&fsize=401184783&hid=3dbd2734afd4234ec9820446a4a43abb&media_type=video&tknv=v3&ts=64929b582d100&s=58d288df6c2a5c5a36fa914fb86358f8d5667b917aabdd9f8bc045043a327872&pb=U2FsdGVkX1_F_dZZX3EcLncmfo3OVrKonuhj8xNC2YQIUQbL8IIOu9_eZJuNpDJRgtXsrga2DFZ7Lh4kdgyF9Kcyvkuxea-WupGTcobTwNo",
     year: 2025,
     genres: ["Acción", "Comedia", "Familiar"],
     info: "Un conductor que trabaja para el crimen organizado se ve obligado a llevar a un joven recluta a una emboscada mortal tras un robo fallido. A medida que su conciencia empieza a pesarle y la tensión aumenta durante el trayecto, el conductor deberá decidir si cumplir con su lealtad al jefe criminal o arriesgarlo todo para salvar la vida de su pasajero en una carrera contra el tiempo.",
