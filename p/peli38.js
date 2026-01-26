f5c6cd451a798d80aabd01c494b9b3450bce7fad
Auraflix <lestheradonys@gmail.com>
2026-01-25 18:50:18 -0600

    Add files via upload

---
+++ p/peli38.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p38",
+    title: "The Home",
+    tags: ["El hogar"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/r0NUt1M62StNgvSLmLsmJADbHpg.jpg",
+    trailer: "https://www.youtube.com/embed/i86P9cBvtoM",
+    video: "https://mixdrop.top/e/z1zd68vztz7elm",
+    year: 2025,
+    genres: ["Misterio", "Terror", "Suspenso"],
+    info: "Max, un joven con problemas que empieza a trabajar en una residencia de ancianos, descubre rápidamente que los residentes y los cuidadores esconden secretos siniestros. A medida que explora los pasillos prohibidos y las áreas restringidas del edificio, se da cuenta de que su conexión con la institución es mucho más profunda de lo que imaginaba, enfrentándose a un horror que desafía toda lógica.",
+    featured: true
+});
