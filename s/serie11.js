d206f570236875b5f6e3ba42f7d9461f5793c539
Auraflix <lestheradonys@gmail.com>
2026-01-31 20:35:51 -0600

    Correct episode data in serie11.js
    Fixed episode entries in the seasons array.

--- s/serie11.js
+++ s/serie11.js
@@ -14,8 +14,8 @@ window.seriesList.push({
         {
             season: 1,
             episodes: [
-                { episode: 1, video: "https://vimeos.net/embed-2jgybzcof3h2.html" }, { episode: 2, video: "https://vimeos.net/embed-mrknbdc8279d.html" }, { episode: 3, video: "" },
-                { episode: , video: "" }, { episode: 5, video: "" }, { episode: 6, video: "" }
+                { episode: 1, video: "https://vimeos.net/embed-2jgybzcof3h2.html" }, { episode: 2, video: "https://vimeos.net/embed-mrknbdc8279d.html" }; { episode: 3, video: "" },
+                { episode: 4, video: "" }, { episode: 5, video: "" }, { episode: 6, video: "" }
             ]
         }
     ]
