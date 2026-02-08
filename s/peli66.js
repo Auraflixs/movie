f8bdd36f0b6517b2d175a1add6beaf964b0ea89d
Auraflix <lestheradonys@gmail.com>
2026-02-07 22:53:33 -0600

    Add files via upload

---
+++ s/peli66.js
@@ -0,0 +1,14 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p66",
+    title: "La empleada",
+    tags: ["The Housemaid", "La asistenta"],
+    image: "https://raw.githubusercontent.com/yoldkwlws/Stranger-things/refs/heads/main/AdvXt9BxxO9XmpJnvduUSoLawJS.jpg",
+    trailer: "https://www.youtube.com/embed/48CtX6OgU3s",
+    video: "https://dropload.pro/e/5ssmlekt584k",
+    year: 2025,
+    genres: ["Misterio", "Suspenso"],
+    info: "Millie Calloway, una joven que intenta reconstruir su vida, acepta un trabajo como empleada doméstica para la adinerada familia Winchester. Sin embargo, pronto descubre que la casa oculta secretos oscuros y que la habitación que le han asignado solo se cierra por fuera. En un juego de manipulación y peligro, Millie deberá descubrir quién es la verdadera víctima antes de que sea demasiado tarde.",
+    featured: true
+});
