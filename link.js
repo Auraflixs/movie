e1567997385ab7b879e4b0321580fe53b3b33087
Auraflix <lestheradonys@gmail.com>
2026-02-07 22:16:10 -0600

    Add files via upload

---
+++ link.js
@@ -0,0 +1,11 @@
+const AURA_LINKS = {
+    // Enlace del Grupo de Telegram (Botón Difusión)
+    telegram: 'https://t.me/Auraflixpeli',
+
+    // Enlace para descargar la App (Botón Compartir App)
+    appDownload: 'https://www.mediafire.com/file/gipq3eknebz7lkn/peli49292.apk/file',
+
+    // Enlace de la página Web (Botón Compartir Web)
+    webPage: 'https://yoldkwlws.github.io/Auraflix2.pk/'
+};
+
