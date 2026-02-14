e680b9abd597dd70e110328d07bf606fa8d56d88
Auraflix <lestheradonys@gmail.com>
2026-02-14 12:40:50 -0600

    Add files via upload

---
+++ p/peli78.js
@@ -0,0 +1,13 @@
+window.moviesList = window.moviesList || [];
+
+window.moviesList.push({
+    id: "p78",
+    title: "Hamnet",
+    image: "https://raw.githubusercontent.com/Auraflixs/Stranger-things/refs/heads/main/61xMzN4h8iLk0hq6oUzr9Ts6GE9.jpg",
+    trailer: "https://www.youtube.com/embed/Ith7z6FzLe0",
+    video: "https://dropload.pro/e/v84kbelgz7mt",
+    year: 2025,
+    genres: ["Drama", "Romance", "Historia"],
+    info: "Ambientada en la Inglaterra de 1580, la película explora la vida de Agnes, la esposa de William Shakespeare, y el profundo dolor que envuelve a su familia tras la pérdida de su hijo Hamnet a causa de la peste. Una historia conmovedora sobre el duelo, la creación artística y el amor que inspiraría una de las obras más famosas de la literatura universal.",
+    featured: true
+});
